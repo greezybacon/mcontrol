f1c16f810c5e9533ed7abe17eb8091d4cc7f4253
Jared Hancock <jared.hancock@manchac.com>
2012-12-18 13:54:32 -0600

    Add support for naming units

---
+++ lib/python/cli/modules/naming.py
@@ -0,0 +1,85 @@
+# coding: utf-8
+"""
+Naming context and commands
+
+Allows for motors in a sub-assembly to be named in manufacturing as well as
+in the field when motors are replaced. As intended, the motors are
+anticipated to be in factory default mode (9600 baud, no address, and no
+party mode).
+"""
+
+from __future__ import print_function
+
+from . import Mixin, trim
+
+from cli import Shell
+from lib import term
+
+import os.path
+import re
+
+class NameCommands(Mixin):
+    def do_name(self, arg):
+        """
+        Usage: name <port>
+
+        Name motors connected to the given serial port. The motors are
+        assumed to use the mdrive driver.
+        """
+        port, speed = arg.partition('@')[::2]
+        if port == "":
+            raise SyntaxError("Serial port must be specified")
+        elif not os.path.exists(port):
+            raise IOError("Serial port specified does not exist")
+
+        NamingContext(port=arg, context=self.context.copy()).run()
+
+    def complete_name(self, text, line, start, end):
+        if line.startswith("/dev/tty"):
+            return glob.glob("/dev/tty[SM]*")
+        else:
+            return ['/dev/tty']
+
+class NamingContext(Shell):
+
+    intro = trim("""
+    +====]> Motor naming
+    +-------------------------------------------------
+    Enter naming instructions in the format of
+
+    <dn>:<sn>
+
+    For example, x:123456789, where <dn> is the name of the device to be
+    named, and <sn> is its serial number
+    """)
+
+    def __init__(self, port, context):
+        super(NamingContext, self).__init__(context)
+        self.context['port'] = port
+
+        self.do_connect("mdrive://{0}".format(port))
+        self.motor = self.context['motors']['!'].motor
+
+    def default(self, line):
+        match = re.match(r'(?P<name>\w):(?P<sn>\d+)', line)
+        if match is None:
+            self.error("Naming format is <addr>:<serial number>",
+                "see 'help naming' for more information")
+        else:
+            name = match.groupdict()['name']
+            sn = match.groupdict()['sn']
+            try:
+                self.out("Setting %s to %s" % (sn, name.lower()))
+                self.motor.name_set(name.lower(), sn)
+                self.info("Motor successfully named")
+                if self.confirm(
+                        "Would you line to set the baudrate to 115.2k? "):
+                    self.motor.baudrate = 115200
+                    pass
+            except AttributeError:
+                self.error("Motor does not support naming")
+            except Exception as e:
+                self.error("{0}: Unable to name motor".format(e.message))
+
+    def help_naming(self):
+        print(self.intro)
