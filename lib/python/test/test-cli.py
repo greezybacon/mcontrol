f77a1b842c57f615be3fcd240442b6237ef913bf
Jared Hancock <jared.hancock@manchac.com>
2013-01-21 11:07:59 -0600

    Allow test-cli.py to be called without cwd assumed

--- lib/python/test/test-cli.py
+++ lib/python/test/test-cli.py
@@ -1,8 +1,12 @@
 from __future__ import print_function
 
-import sys, glob
-sys.path.insert(0, '../cli')
-for lib in glob.glob('../build/lib.linux-*-{0}.{1}'.format(
+import sys, glob, os
+
+install_path = os.path.abspath(
+    os.path.dirname(os.path.abspath(__file__)) + '/..')
+
+sys.path.insert(0, os.path.abspath(install_path + '/cli'))
+for lib in glob.glob(install_path + '/build/lib.linux-*-{0}.{1}'.format(
         sys.version_info.major, sys.version_info.minor)):
     sys.path.insert(0, lib)
 
