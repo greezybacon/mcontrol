d618223371a800b81718d7069b96c170b0f5f88f
Jared Hancock <jared.hancock@manchac.com>
2013-02-27 07:54:34 -0600

    Support build tree and new event interface

--- lib/python/test/test-move.py
+++ lib/python/test/test-move.py
@@ -1,12 +1,22 @@
 from __future__ import print_function
 
-from mcontrol import *
-f=Motor('mdrive:///dev/ttyS0')
-def stall(ev):
- print("Motor stalled")
+import sys, glob
+sys.path.insert(0, '../cli')
+for lib in glob.glob('../build/lib.linux-*-{0}.{1}'.format(
+        sys.version_info.major, sys.version_info.minor)):
+    sys.path.insert(0, lib)
 
-ev = f.on(EV_MOTOR_STALLED, stall)
-f.units=MILLI_INCH
-f.move(1)
+from mcontrol import *
+f=Motor('mdrive:///dev/ttyS0@115200:a')
+def stall(ev):
+    if ev.data.stalled:
+        print("Motor stalled")
+
+ev = f.on(Event.EV_MOTION)
+ev.call(stall)
+f.units=all_units['mil']
+f.scale=1e6
+
+f.move(14)
 
 ev.wait()
